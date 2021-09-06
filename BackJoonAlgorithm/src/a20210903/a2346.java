4971a14b3ed6c76c65238194ad62d190761185d6
Lee, Jaehyeok <dlwogur15@gmail.com>
2021-09-07 04:28:32 +0900

    commit test

---
+++ BackJoonAlgorithm/src/a20210903/a2346.java
@@ -0,0 +1,40 @@
+package a20210903;
+
+import java.util.*;
+
+
+
+public class a2346 {
+	
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		Deque<Integer> que = new LinkedList<>();
+		ArrayList<Integer> list = new ArrayList<>();
+		
+		for(int i = 0; i < n; i++) {
+			que.addLast(sc.nextInt());
+		}
+		
+		int num = que.peek();
+		int i = 0;
+		
+		
+		while(!que.isEmpty()) {
+			
+			
+			
+			
+			
+		}
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
