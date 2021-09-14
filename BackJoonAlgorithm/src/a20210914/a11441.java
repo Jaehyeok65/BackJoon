2fc8b15e8c53cf7938b6f2ee0c64cd4d4d63e2a5
Lee, Jaehyeok <dlwogur15@gmail.com>
2021-09-14 20:10:03 +0900

    2021 09 14 백준 11441 합 구하기 (누적 합)

--- BackJoonAlgorithm/src/a20210914/a11441.java
+++ BackJoonAlgorithm/src/a20210914/a11441.java
@@ -6,6 +6,7 @@ import java.util.*;
 
 public class a11441 {
 	static int arr[];
+	static int result[];
 	
 	public static int solution(int a, int b) {
 		if(a == b) {
@@ -17,6 +18,16 @@ public class a11441 {
 		}
 		return sum;
 	}
+	
+	public static int sol(int a, int b) { 
+		if(a == b) {
+			return arr[a];
+		}
+		
+		int ans = result[b] - result[a-1];
+		
+		return ans;
+	}
 
 	public static void main(String[] args) {
 		
@@ -26,9 +37,13 @@ public class a11441 {
 		int n = sc.nextInt();
 		
 		arr = new int[n+1];
+		result = new int[n+1];
+		int ans = 0;
 		
 		for(int i = 1; i <= n; i++) {
 			arr[i] = sc.nextInt();
+			ans += arr[i];
+			result[i] = ans;
 		}
 		
 		int m = sc.nextInt();
@@ -36,7 +51,7 @@ public class a11441 {
 		for(int i = 0; i < m; i++) {
 			int a = sc.nextInt();
 			int b = sc.nextInt();
-			sb.append(solution(a,b)+"\n");
+			sb.append(sol(a,b)+"\n");
 		}
 		System.out.print(sb.toString());
 		
