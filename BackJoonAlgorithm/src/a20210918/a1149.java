8be92836afd6336216d273ca616d5326edf22acd
Lee, Jaehyeok <dlwogur15@gmail.com>
2021-09-18 13:56:59 +0900

    2021 09 18 백준 1149 RGB거리 (다이나믹 프로그래밍)

---
+++ BackJoonAlgorithm/src/a20210918/a1149.java
@@ -0,0 +1,42 @@
+package a20210918;
+
+
+import java.util.*;
+
+
+public class a1149 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int arr[][] = new int[n][3];
+		int dp[][] = new int[n][3];
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < 3; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		dp[0][0] = arr[0][0];
+		dp[0][1] = arr[0][1];
+		dp[0][2] = arr[0][2];
+		
+		for(int i = 1; i < n; i++) {
+			dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + arr[i][0];
+			dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + arr[i][1];
+			dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + arr[i][2];
+		}
+		
+		int result = Math.min(Math.min(dp[n-1][0], dp[n-1][1]), dp[n-1][2]);
+		
+		System.out.print(result);
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
