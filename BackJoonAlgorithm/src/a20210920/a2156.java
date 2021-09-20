ac7b07b3ad46fb9e8084bb66351e1dda727c75e2
Lee, Jaehyeok <dlwogur15@gmail.com>
2021-09-20 11:22:42 +0900

    2021 09 20 백준 2156 포도주 시식 (다이나믹 프로그래밍)

---
+++ BackJoonAlgorithm/src/a20210920/a2156.java
@@ -0,0 +1,35 @@
+package a20210920;
+
+
+import java.util.*;
+
+
+public class a2156 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int arr[] = new int[n+1];
+		int dp[] = new int[n+1];
+		for(int i = 1; i <= n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		dp[1] = arr[1];
+		if(n > 1) {
+		dp[2] = arr[1] + arr[2];
+		}
+		
+		for(int i = 3; i <= n; i++) { //여기부터는 3번연속인지를 신경써야함.
+			dp[i] = Math.max(dp[i-1], Math.max(dp[i-2] + arr[i], dp[i-3] + arr[i-1] + arr[i]));
+		}
+		
+		System.out.print(dp[n]);
+		// TODO Auto-generated method stub
+
+	}
+
+}
